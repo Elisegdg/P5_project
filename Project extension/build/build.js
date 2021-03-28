4708919024b9460d5e12cccb1d43cd472acabcd7
Elisegdg <elise@gondange.com>
2021-03-28 11:10:47 +0200

    Ajout extension projet

---
+++ Project extension/build/build.js
@@ -0,0 +1,95 @@
+var gui = new dat.GUI();
+var params = {
+    Ellipse_Size: 30,
+    Download_Image: function () { return save(); },
+};
+gui.add(params, "Ellipse_Size", 0, 100, 1);
+gui.add(params, "Download_Image");
+function draw() {
+    angleMode(DEGREES);
+    translate(width / 2, height / 2);
+    background('white');
+    strokeWeight(1);
+    color('black');
+    var value = 300;
+    var radius = 26;
+    for (var nbCercles = 0; nbCercles < 6; nbCercles++) {
+        for (var alpha_1 = 0; alpha_1 <= 360; alpha_1 += 10) {
+            push();
+            rotate(alpha_1);
+            beginShape();
+            for (var i = 0; i < 8; i++) {
+                var angle = i / 8 * 360;
+                vertex(cos(angle) * radius + value, sin(angle) * radius);
+            }
+            endShape(CLOSE);
+            pop();
+        }
+        if (radius > 0) {
+            value = value - radius * 1.8;
+            radius = radius - 4;
+        }
+        else {
+            value = value - radius * 5;
+            radius = radius - 1;
+        }
+    }
+}
+function setup() {
+    p6_CreateCanvas();
+}
+function windowResized() {
+    p6_ResizeCanvas();
+}
+var __ASPECT_RATIO = 1;
+var __MARGIN_SIZE = 25;
+function __desiredCanvasWidth() {
+    var windowRatio = windowWidth / windowHeight;
+    if (__ASPECT_RATIO > windowRatio) {
+        return windowWidth - __MARGIN_SIZE * 2;
+    }
+    else {
+        return __desiredCanvasHeight() * __ASPECT_RATIO;
+    }
+}
+function __desiredCanvasHeight() {
+    var windowRatio = windowWidth / windowHeight;
+    if (__ASPECT_RATIO > windowRatio) {
+        return __desiredCanvasWidth() / __ASPECT_RATIO;
+    }
+    else {
+        return windowHeight - __MARGIN_SIZE * 2;
+    }
+}
+var __canvas;
+function __centerCanvas() {
+    __canvas.position((windowWidth - width) / 2, (windowHeight - height) / 2);
+}
+function p6_CreateCanvas() {
+    __canvas = createCanvas(__desiredCanvasWidth(), __desiredCanvasHeight());
+    __centerCanvas();
+}
+function p6_ResizeCanvas() {
+    resizeCanvas(__desiredCanvasWidth(), __desiredCanvasHeight());
+    __centerCanvas();
+}
+var p6_SaveImageSequence = function (durationInFrames, fileExtension) {
+    if (frameCount <= durationInFrames) {
+        noLoop();
+        var filename_1 = nf(frameCount - 1, ceil(log(durationInFrames) / log(10)));
+        var mimeType = (function () {
+            switch (fileExtension) {
+                case 'png':
+                    return 'image/png';
+                case 'jpeg':
+                case 'jpg':
+                    return 'image/jpeg';
+            }
+        })();
+        __canvas.elt.toBlob(function (blob) {
+            p5.prototype.downloadFile(blob, filename_1, fileExtension);
+            setTimeout(function () { return loop(); }, 100);
+        }, mimeType);
+    }
+};
+//# sourceMappingURL=../src/src/build.js.map
